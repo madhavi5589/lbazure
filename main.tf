22722d0e334b7a02adb2217380b94212f1d6ace5
Ubuntu <azureuser@jenkins-server.idaq3edelshuboecqfdywpymmc.bx.internal.cloudapp.net>
2023-11-29 07:01:47 +0000

    first2

--- main.tf
+++ main.tf
@@ -11,22 +11,22 @@ terraform {
 provider "azurerm" {
   features {}
 }
-#resource "azurerm_resource_group" "example" {
- # name     = var.resource_group_name
-  #location = var.location
-#}
+data "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
 resource "azurerm_public_ip" "example" {
   name                = var.azurerm_public_ip
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   allocation_method   = "Static"
 }
 
 resource "azurerm_lb" "example" {
   name                = var.azurerm_lb
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   frontend_ip_configuration {
     name                 = var.frontend_ip_configuration
