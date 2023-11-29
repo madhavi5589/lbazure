f7c7c73874230ab7eda353088313b175351d90ae
Ubuntu <azureuser@jenkins-server.idaq3edelshuboecqfdywpymmc.bx.internal.cloudapp.net>
2023-11-29 07:07:59 +0000

    first4

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "resource_group_name" {
   description = "The name of the resource group in which to create the AKS cluster."
   type        = string
 }
-variable "location" {
-  description = "The name of the resource group in which to create the AKS cluster."
-  type        = string
-}
 
 variable "azurerm_public_ip" {
 
